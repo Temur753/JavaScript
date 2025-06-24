6b00a77d43b5cc802c4bb7b39b68d37242b4008a
Temur753 <akhtamovtemur378@gmail.com>
2025-06-24 11:06:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let ism = prompt("Ismingizni kiriting")
+
+let familiya = prompt("Familiyangizni kiriting")
+
+let yosh = prompt("Yoshingizni kiriting")
+
+let fan = prompt("Yoqtirgan faningizni kiriting")
+
+let manzil = prompt("Yashash manzilingizni kiriting")
+
+console.log("Ism: " + ism)
+console.log("Familiya: " + familiya)
+console.log("Yqtirgan fan: " + fan)
+console.log("Yashash manzili: " + manzil)
